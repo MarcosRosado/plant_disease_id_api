669387c607df31684827cf11a20ad3efee94d8a6
Marcos Rosado <marcos@tacticup.com.br>
2024-08-30 20:04:06 -0300

    added github secret configuration

--- src/setupFirebaseKey.ts
+++ src/setupFirebaseKey.ts
@@ -1,5 +1,5 @@
-// setupFirebaseKey.ts
 import fs from 'fs';
+import path from 'path';
 
 const firebaseKey = process.env.FIREBASE_KEY;
 
@@ -7,4 +7,11 @@ if (!firebaseKey) {
   throw new Error('FIREBASE_KEY is not defined in environment variables');
 }
 
-fs.writeFileSync('src/key/plant-analysis-key.json', firebaseKey);
+const dir = path.join(__dirname, 'key');
+const filePath = path.join(dir, 'plant-analysis-key.json');
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+fs.writeFileSync(filePath, firebaseKey);
